605665a0a47bc6df5f7e18ab332209d6dd4f0913
Beginner76 <alnasiriyusuf@gmail.com>
2023-06-18 20:25:17 +0300

    finished form

--- app/main.js
+++ app/main.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const colorForm = document.getElementById("color-form")
+colorForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const colorInput = document.getElementById("color-input")
+    const schemeInput = document.getElementById("scheme-input")
+    console.log(colorInput.value) 
+    console.log(schemeInput.value) 
+})
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,7 +3,14 @@ export default {
   content: ["./index.html",
   "./public/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow:{
+        "formEl": "0px 1px 2px rgba(0, 0, 0, 0.05)" 
+      },
+      fontFamily:{
+        "inter": "'Inter', sans-serif"
+      }
+    },
   },
   plugins: [],
 }
